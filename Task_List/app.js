97c0bd806949efd55d4ee3573ce339ac4b7cf405
jerry047 <jerry00247@gmail.com>
2020-03-30 15:05:06 +0545

    add function delete and filter task

--- Task_List/app.js
+++ Task_List/app.js
@@ -13,6 +13,15 @@ loadEventListeners();
 function loadEventListeners(){
     //Add task Event
     form.addEventListener('submit', addTask);
+
+    //Remove task event
+    taskList.addEventListener('click', removeTask);
+
+    //Clear task event
+    clearBtn.addEventListener('click', clearTasks);
+
+    //Filter through the task events
+    filter.addEventListener('keyup', filterTasks);
 }
 
  // Creating addTask function
@@ -35,7 +44,7 @@ function loadEventListeners(){
 
     //ADD class
     link.className = 'delete-item secondary-content';
-    
+
     //ADD icon html
     link.innerHTML = '<i class="fa fa-remove"></i>';
 
@@ -50,3 +59,39 @@ function loadEventListeners(){
 
     e.preventDefault();
  }
+
+ //Creating removetask function
+  function removeTask(e){
+    if(e.target.parentElement.classList.contains('delete-item')){
+        if(confirm('Are you sure?')){
+        e.target.parentElement.parentElement.remove();
+        }
+    }
+  }
+
+  //Creating clearTasks function
+  function clearTasks(){
+      //simple way to do it
+      taskList.innerHTML = '';
+
+      //Faster way to do it
+      while(taskList.firstChild){
+          taskList.removeChild(taskList.firstChild);
+      }
+  }
+
+  //Creating filterTasks function
+  function filterTasks(e){
+      const text = e.target.value.toLowerCase();
+
+      document.querySelectorAll('.collection-item').forEach(
+          function(task){
+            const item = task.firstChild.textContent;
+            if(item.toLowerCase().indexOf(text) != -1){
+                task.style.display = 'block';
+            } else {
+                task.style.display = 'none';
+            }
+      });
+
+  }
