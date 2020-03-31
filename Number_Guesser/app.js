70527cc624e93e7cb8a3a8ad31bc9d4978d395e2
jerry047 <jerry00247@gmail.com>
2020-03-31 15:15:28 +0545

    play again feature add

--- Number_Guesser/app.js
+++ Number_Guesser/app.js
@@ -25,6 +25,13 @@ const game = document.querySelector('#game'),
 minNum.textContent = min;
 maxNum.textContent = max;
 
+//Play again event listener
+game.addEventListener('mousedown', function(e){
+    if(e.target.className === 'play-again'){
+        window.location.reload();
+    }
+})
+
 //Listen for guess
 guessBtn.addEventListener('click', function () {
     let guess = parseInt(guessInput.value);
@@ -76,6 +83,10 @@ guessBtn.addEventListener('click', function () {
      message.style.color = color;
      //Set message if won
      setMessage(msg);
+
+     //Play again
+     guessBtn.value = 'Play Again';
+     guessBtn.className += 'play-again';
  }
 
 //Set message
