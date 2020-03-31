e80162fedce6eeb7751a3e8cbf857ec8555032d1
jerry047 <jerry00247@gmail.com>
2020-03-31 15:23:49 +0545

    random winning value create

--- Number_Guesser/app.js
+++ Number_Guesser/app.js
@@ -10,7 +10,7 @@ GAME FUNCTION:
 //Game values
 let min = 1,
     max = 10,
-    winningNum = 2,
+    winningNum = getRandomNum(min, max),
     guessesLeft = 3;
 
 //UI elements
@@ -87,6 +87,11 @@ guessBtn.addEventListener('click', function () {
      //Play again
      guessBtn.value = 'Play Again';
      guessBtn.className += 'play-again';
+ }
+
+ //Getting random number
+ function getRandomNum(min, max){
+     return Math.floor(Math.random()*(max-min+1)+min);
  }
 
 //Set message
