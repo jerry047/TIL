65d65c9a78d35a799596052872457015ef30b018
jerry047 <jerry00247@gmail.com>
2020-04-01 14:56:10 +0545

    feature added: remove add and display from local storage

--- BookList_Project/appes6.js
+++ BookList_Project/appes6.js
@@ -53,6 +53,48 @@ class UI {
     }
 }
 
+//Local Storage Class
+class Store{
+    static getBooks(){
+        let books;
+        if(localStorage.getItem('books') === null){
+            books = [];
+        }else {
+            books = JSON.parse(localStorage.getItem('books'));
+        }
+        return books;
+    }
+
+    static displayBooks(){
+        const books = Store.getBooks();
+        books.forEach(function(book){
+            const ui = new UI;
+
+            //Add book to ui
+            ui.addBookToList(book);
+          });
+    }
+
+    static addBook(book){
+        const books = Store.getBooks();
+        books.push(book);
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+
+    static removeBook(isbn){
+        const books = Store.getBooks();
+        books.forEach(function(book, index){
+            if(book.isbn === isbn){
+                books.splice(index, 1)
+            }
+          });
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+}
+
+//Dom load event
+document.addEventListener('DOMContentLoaded', Store.displayBooks);
+
 //Event Listener for add book
 document.getElementById('book-form').addEventListener('submit', function(e){
 
@@ -76,6 +118,9 @@ document.getElementById('book-form').addEventListener('submit', function(e){
     // Add book list to UI
     ui.addBookToList(book);
 
+    // Add to LS
+    Store.addBook(book);
+
     // Show Success
     ui.showAlert('Book Added', 'success');
 
@@ -94,6 +139,9 @@ document.getElementById('book-list').addEventListener('click', function(e){
 
     // Delete Book
     ui.deleteBook(e.target);
+
+    //REmove from LS
+    Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
 
     //Show delete alert
     ui.showAlert('Book Removed!', 'success');
