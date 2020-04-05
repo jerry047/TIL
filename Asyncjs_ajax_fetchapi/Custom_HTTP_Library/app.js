2ea345d33111a2eb5eb962faabe12b5a3a880ee8
jerry047 <jerry00247@gmail.com>
2020-04-05 12:06:26 +0545

    add PUT method to update data through library

--- Asyncjs_ajax_fetchapi/Custom_HTTP_Library/app.js
+++ Asyncjs_ajax_fetchapi/Custom_HTTP_Library/app.js
@@ -24,3 +24,11 @@ http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post
     } 
 })
 
+// Update Post
+http.put('https://jsonplaceholder.typicode.com/posts/1', data, function(err, post){
+    if(err) {
+        console.log(err); 
+    }else {
+        console.log(post);
+    }  
+})
