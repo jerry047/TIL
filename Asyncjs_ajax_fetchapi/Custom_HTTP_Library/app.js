40ce661ed17e306e44fa7f804fa8e717d1d9cace
jerry047 <jerry00247@gmail.com>
2020-04-05 12:02:45 +0545

    add post method in custom http library

--- Asyncjs_ajax_fetchapi/Custom_HTTP_Library/app.js
+++ Asyncjs_ajax_fetchapi/Custom_HTTP_Library/app.js
@@ -8,3 +8,19 @@ http.get('https://jsonplaceholder.typicode.com/posts', function(err, response){
         console.log(response);
     }  
 });
+
+//Create Data
+const data = {
+    title: 'Custom-post',
+    body: 'this is a custom post'
+};
+
+//Create post
+http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post){
+    if(err) {
+        console.log(err);
+    }else {
+        console.log(post);
+    } 
+})
+
