d078e51ce5c07887ac4eb6d1aeb37a26a4e07f28
jerry047 <jerry00247@gmail.com>
2020-04-06 16:53:15 +0545

    method add to store data locally in local storage

--- WeatherApp/app.js
+++ WeatherApp/app.js
@@ -1,7 +1,11 @@
+//Init local storage
+const storage = new Storage();
+//Get Stored location data
+const weatherLocation = storage.getLocationData();
 //Init weather object
-const weather = new Weather('London', 'uk');
+const weather = new Weather(weatherLocation.city, weatherLocation.state);
 //Init UI object
-const ui = new UI;
+const ui = new UI();
 
 //Get weather on Dom load
 document.addEventListener('DOMContentLoaded', getWeather);
@@ -11,7 +15,11 @@ document.getElementById('w-change-btn').addEventListener('click', (e) => {
     const city = document.getElementById('city').value;
     const state = document.getElementById('state').value;
 
+    //Change location
     weather.changeLocation(city, state);
+
+    //Set location to storage
+    storage.setLocationData(city, state);
 
     //Get and display weather
     getWeather();
---
+++ WeatherApp/storage.js
@@ -0,0 +1,31 @@
+class Storage {
+    constructor() {
+        this.city;
+        this.state; 
+        this.defaultCity = 'Miami';
+        this.defaultState = 'Florida';
+    }
+
+    getLocationData() {
+        if(localStorage.getItem('city') === null) {
+            this.city = this.defaultCity;
+        } else {
+            this.city = localStorage.getItem('city');
+        }
+        if(localStorage.getItem('state') === null) {
+            this.city = this.defaultState;
+        } else {
+            this.city = localStorage.getItem('state');
+        }
+
+        return {
+            city: this.city,
+            state: this.state
+        }
+    }
+
+    setLocationData(city, state) {
+        localStorage.setItem('city', city);
+        localStorage.setItem('state', state);
+    }
+}
