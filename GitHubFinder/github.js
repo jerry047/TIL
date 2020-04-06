ab97efe64943f4edcf42c2283494802ff238d7ac
jerry047 <jerry00247@gmail.com>
2020-04-06 13:27:33 +0545

    fetch user repos and display in ui

--- GitHubFinder/github.js
+++ GitHubFinder/github.js
@@ -2,15 +2,20 @@ class Github {
     constructor() {
         this.client_id = 'a734615e211d3c2b036d';
         this.client_secret = 'b033810c165fc871d5ad69c8d80e163aa6354650';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc'
     }
 
     async getUser(user) {
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
         const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
 
         return {
-             profile
+             profile,
+             repos
         }
     }
 }
