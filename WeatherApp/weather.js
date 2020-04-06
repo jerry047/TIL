f93166dbe054f3f47183f6597a748e1219c4ca4b
jerry047 <jerry00247@gmail.com>
2020-04-06 15:07:17 +0545

    create async method to fetch weather info and create changelocation method

---
+++ WeatherApp/weather.js
@@ -0,0 +1,22 @@
+class Weather {
+    constructor(city, state){
+        this.apiKey = 'f5c69d72161c323084097c5cb0564fb1';
+        this.city = city;
+        this.state = state;
+    }
+
+    //Fetch weather from api
+    async getWeather() {
+        const response = await fetch(`api.openweathermap.org/data/2.5/weather?q=${this.state},${this.city}&APPID=${this.apiKey}`);
+
+        const responseData = await response.json();
+
+        return responseData;
+    }
+
+    //Change the weather location
+    changeLocation(city, state) {
+        this.city = city;
+        this.state = state;
+    }
+}
