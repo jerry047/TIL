eb4dfd757d27b6b8f40c0ace447e0709708294f8
jerry047 <jerry00247@gmail.com>
2020-04-06 15:41:29 +0545

    call getweather on dom load

--- WeatherApp/app.js
+++ WeatherApp/app.js
@@ -1,10 +1,16 @@
 //Init weather object
 const weather = new Weather('London', 'uk');
 
-weather.changeLocation('Miami', 'Florida');
+//Get weather on Dom load
+document.addEventListener('DOMContentLoaded', getWeather);
 
- weather.getWeather()
+// weather.changeLocation('Miami', 'Florida');
+
+function getWeather() {
+    weather.getWeather()
     .then(results => {
         console.log(results)
     })
     .catch(err => console.log(err))
+} 
+ 
