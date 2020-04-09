9ed4d2b19dcc07ffe3c32ab5114bb32228987b73
jerry047 <jerry00247@gmail.com>
2020-04-09 16:54:48 +0545

    create controllers and data structure template

---
+++ Track_calorie/app.js
@@ -0,0 +1,53 @@
+//Storage Controller
+
+//Item Controller
+const ItemCntrl = (function() {
+    //Item Constructor
+    const Item = function(id, name, calories) {
+        this.id = id;
+        this.name = name;
+        this.calories = calories;
+    }
+
+    //Data Structure / State
+    const data = {
+        items: [
+            {id: 0, name: 'Steak Dinner', calories: '1200'},
+            {id: 1, name: 'Cookie', calories: '400'},
+            {id: 2, name: 'Eggs', calories: '300'}
+        ],
+        currentItem: null,
+        totalCalories: 0
+    }
+
+    return {
+        logdata: function() {
+            return data;
+        }
+    }
+})();
+
+//UI Controller
+const UICntrl = (function() {
+
+    //Public methods
+    return {
+
+    }
+    
+})();
+
+//App Controller
+const App = (function(ItemCntrl, UICntrl) {
+
+    //Public methods
+    return {
+        init: function() {
+            console.log('Initializing app');
+        }
+    }
+    
+})(ItemCntrl, UICntrl);
+
+//Initialize app
+App.init();
