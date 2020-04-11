39224ca627d7b0d98e2ed113e360f5f6536fa200
jerry047 <jerry00247@gmail.com>
2020-04-11 13:04:05 +0545

    add post to db and show alert

--- microposts/src/app.js
+++ microposts/src/app.js
@@ -2,10 +2,34 @@ import { http } from './http';
 import { ui } from './ui';
 
 //Get posts on DOM load
-document.addEventListener('DOMContentLoaded', getpost);
+document.addEventListener('DOMContentLoaded', getPosts);
 
-function getpost() {
+//Listen for add post
+document.querySelector('.post-submit').addEventListener('click', submitPost);
+
+//Get posts
+function getPosts() {
   http.get('http://localhost:3000/posts')
   .then(data => ui.showPosts(data))
   .catch(err => console.log(err));
+}
+
+//Submit post
+function submitPost() {
+  const title = document.querySelector('#title').value;
+  const body = document.querySelector('#body').value;
+
+  const data = {
+    title,
+    body
+  }
+
+  //Create post
+  http.post('http://localhost:3000/posts', data)
+    .then(data => {
+      ui.showAlert('Post Added', 'alert alert-success');
+      ui.clearFields();
+      getPosts();
+    })
+    .catch(err => console.log(err))
 }
