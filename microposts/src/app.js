f4bf63032585360851753130088c19edfd9dafa9
jerry047 <jerry00247@gmail.com>
2020-04-11 14:59:34 +0545

    create method to update post and change state

--- microposts/src/app.js
+++ microposts/src/app.js
@@ -13,6 +13,9 @@ document.querySelector('#posts').addEventListener('click', deletePost);
 //Listen for edit state
 document.querySelector('#posts').addEventListener('click', enableEdit);
 
+//Listen for cancel
+document.querySelector('.card-form').addEventListener('click', cancelEdit);
+
 //Get posts
 function getPosts() {
   http.get('http://localhost:3000/posts')
@@ -24,20 +27,42 @@ function getPosts() {
 function submitPost() {
   const title = document.querySelector('#title').value;
   const body = document.querySelector('#body').value;
+  const id = document.querySelector('#id').value;
 
   const data = {
     title,
     body
   }
 
-  //Create post
-  http.post('http://localhost:3000/posts', data)
-    .then(data => {
-      ui.showAlert('Post Added', 'alert alert-success');
-      ui.clearFields();
-      getPosts();
-    })
-    .catch(err => console.log(err))
+  //Validate input field is not empty
+  if(title === '' || body === '') {
+    ui.showAlert('Please fill in all fields', 'alert alert-danger');
+  } else {
+    
+    //Check for id
+    if(id === '') {
+       //Create post
+      http.post('http://localhost:3000/posts', data)
+      .then(data => {
+        ui.showAlert('Post Added', 'alert alert-success');
+        ui.clearFields();
+        getPosts();
+      })
+      .catch(err => console.log(err))
+
+    } else {
+      //Update post
+      http.put(`http://localhost:3000/posts/${id}`, data)
+      .then(data => {
+        ui.showAlert('Post Updated', 'alert alert-success');
+        ui.changeFormState('add');
+        getPosts();
+      })
+      .catch(err => console.log(err))
+    }
+  
+   
+  }
 }
 
 //Delete post
@@ -72,6 +97,14 @@ function enableEdit(e) {
 
     //Fill form with current post
     ui.fillForm(data);
+  }
+  e.preventDefault();
+}
+
+//Cancel edit state
+function cancelEdit(e) {
+  if(e.target.classList.contains('post-cancel')) {
+    ui.changeFormState('add');
   }
   e.preventDefault();
 }
